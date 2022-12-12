5042694a80644e83d021c3e0c0ec37b3c80f6db0
e <esmurf@gmail.com>
2022-12-12 16:02:29 +0100

    moving def state

--- 1 nocreds/FindDCip.sh
+++ 1 nocreds/FindDCip.sh
@@ -2,10 +2,10 @@
 echo " "
 echo "Syntax: FindDCip 'interface' 'domain name'" 
 echo "Example: ./FindDCip.sh eth0 mydomain.local"
-rm FindDCip_* # For dev only
 echo "Finding DC IP address..."
 nmcli dev show "$1" | tee FindDCip_"$1".txt
 nslookup -type=SRV _ldap._tcp.dc._msdcs."$2" | tee FindDCip_"$2".txt
 echo "Results: "
 cat FindDCip_"$1".txt
 cat FindDCip_"$2".txt
+rm FindDCip_* # For dev only
