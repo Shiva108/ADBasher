d557594b48c21621ccb7d24fe5e56309f775448f
e <esmurf@gmail.com>
2022-12-14 14:49:27 +0100

    Added nse support

--- 1 nocreds/ADnetscan.sh
+++ 1 nocreds/ADnetscan.sh
@@ -5,24 +5,26 @@ echo "$subnetStr" # for dev only
 echo " "
 echo "Syntax: ADnetscan.sh 'IP range' 'domain'" 
 echo "Example: ./ADnetscan.sh 192.168.123.1/24 domain.local"
-ecgi " 'Domain' is optional for most scans"
+echo " 'Domain' is optional for most scans"
 echo "Scanning network..."
 echo " "
 echo "Enumerating smb hosts"
 # crackmapexec smb — gen-relay-list smb_targets_"$subnetStr".txt "$1"
 echo " "
 echo "Finding vulnerable hosts with nmap"
-# grc nmap -sP "$1" -oA nmap_"$subnetStr"_ping # ping scan
-# grc nmap -sV -Pn --top-ports 50 --open "$1" -oA nmap_"$subnetStr"_quick # quick scan
-# grc nmap -Pn --script smb-vuln* -p139,445 "$1" -oA nmap_"$subnetStr"_smbvuln # search vuln scan
-# grc nmap -sU -sC -sV "$1" -oA nmap_"$subnetStr"_udp # udp scan 
+grc nmap -sP "$1" -oA nmap_"$subnetStr"_ping # ping scan
+grc nmap -sV -Pn --top-ports 50 --open "$1" -oA nmap_"$subnetStr"_quick # quick scan
+grc nmap -Pn --script smb-vuln* -p139,445 "$1" -oA nmap_"$subnetStr"_smbvuln # search vuln scan
+grc nmap -sU -sC -sV "$1" -oA nmap_"$subnetStr"_udp # udp scan 
 # Uncomment to include full scan:
-#grc nmap -sSCV -Pn -p- -T4 -vv --version-intensity 5 --script=banner --max-retries 3 --version-all -oA $1 $1
-rm nmap_"$subnetStr"* # for dev only
+# grc nmap -sSCV -Pn -p- -T4 -vv --version-intensity 5 --script=banner --max-retries 3 --version-all -oA $1 $1
+ rm nmap_"$subnetStr"* # for dev only
+
 # Additional nmap information gathering using nse
-sudo nmap -p 3389 --script rdp-ntlm-info "$subnetStr"
-sudo nmap -sSC -Pn --script=*-ntlm-info -p 23,25,80,3389 "$subnetStr"
-sudo nmap -sSCV --script=*-ntlm-info --script-args http-ntlm-info.root=/ews/ -p 443,587,993 "$2"
+
+# sudo nmap -p 3389 --script rdp-ntlm-info "$2"
+# sudo nmap -sSC -Pn --script=*-ntlm-info -p 23,25,80,3389 "$2"
+# sudo nmap -sSCV --script=*-ntlm-info --script-args http-ntlm-info.root=/ews/ -p 443,587,993 "$2"
 
 
 
