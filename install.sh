93bc0dbbefb3a0f03ae9c01cd2ac477d076be3a3
e <esmurf@gmail.com>
2022-12-20 17:25:33 +0100

    Added rainbow.sh for xr09

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ if [ "$EUID" -ne 0 ]
 fi
 
 echo "ADBasher Installer, tested on Kali and Parrot"
-sudo apt update && apt autoremove && apt autoclean 
+sudo apt install --fix-missing -y
+sudo apt update && apt autoremove && apt autoclean
 echo "Current OS version is:"
 sudo uname -a
-sudo apt install grc crackmapexec 
+sudo apt install grc crackmapexec
+sudo chmod +x ./*.sh
+sudo cat ./README.md | more
---
+++ notes.md
@@ -0,0 +1,10 @@
+# Servers
+
+DC
+192.168.123.22
+Host: SERVER2016
+
+Cert Auth
+192.168.123.12
+Windows Server 2008 R2 - 2012
+
