752c15f31b2a509bfb9fd2488b69f5e5437de071
sh1v4x6c <shiva108@protonmail.com>
2023-11-21 15:34:17 +0100

    updates to smb scan

--- 1 nocreds/smbscan.sh
+++ 1 nocreds/smbscan.sh
@@ -1,52 +1,68 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+
+# Function to display usage
+usage() {
+    echo -e "Usage: $0 'DC IP address'"
+    echo -e "Example: $0 10.10.10.10"
+}
+
+# Check for root privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
 fi
 
-if [ $# -eq 0 ]
-  then
-    echo ""
-    echo "Syntax: smbscan 'DC IP address'" 
-    echo "Example: ./smbscan.sh 10.10.10.10"
+# Validate input parameters
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
 fi
 
-echo " "
-echo "Starting nmap smb for " "$1"
-echo "=========================="
-# grc nmap -p 445 --script=smb-enum-shares.nse,smb-enum-users.nse "$1"
-# grc nmap -p 111 --script=nfs-ls,nfs-statfs,nfs-showmount "$1"
+# Validate IP address format
+if ! echo "$1" | grep -E -q "^([0-9]{1,3}\.){3}[0-9]{1,3}$"; then
+    echo "Invalid IP address format."
+    usage
+    exit 1
+fi
 
-echo " "
-echo "Starting enum4linux for" "$1"
-echo "=========================="
-# enum4linux -U "$1" 
-# enum4linux -a "$1" 
-# enum4linux -a -u "" -p "" "$1" && enum4linux -a -u "guest" -p "" "$1"
+TARGET="$1"
+OUTPUT_FILE="smbscan_results_$TARGET.txt"
 
-echo "Trying to login with smbclient with anonymous and guest for" "$1"
-echo "=========================="
-smbclient //"$1"/anonymous
-smbclient //"$1"/guest
-smbclient --no-pass -L //"$1"
+# Function to perform scans
+perform_scan() {
+    echo -e "\nStarting scans for $TARGET"
+    echo "=========================="
+    echo -e "nmap SMB and NFS scans for $TARGET" | tee -a "$OUTPUT_FILE"
+    grc nmap -p 445 --script=smb-enum-shares.nse,smb-enum-users.nse "$TARGET" | tee -a "$OUTPUT_FILE"
+    grc nmap -p 111 --script=nfs-ls,nfs-statfs,nfs-showmount "$TARGET" | tee -a "$OUTPUT_FILE"
 
-echo " "
-echo "Running smbmap for " "$1"
-echo "=========================="
-smbmap -H "$1"
-smbmap -u '' -p '' -P 445 -H "$1"
-smbmap -u 'guest' -p '' -P 445 -H "$1"
-smbmap -u '' -p '' -H "$1" -R
+    echo -e "\nenum4linux scans for $TARGET" | tee -a "$OUTPUT_FILE"
+    enum4linux -U "$TARGET" | tee -a "$OUTPUT_FILE"
+    enum4linux -a "$TARGET" | tee -a "$OUTPUT_FILE"
+    enum4linux -a -u "" -p "" "$TARGET" | tee -a "$OUTPUT_FILE"
+    enum4linux -a -u "guest" -p "" "$TARGET" | tee -a "$OUTPUT_FILE"
 
-echo " "
-echo "Running nbtscan for " "$1"
-echo "=========================="
-nbtscan "$1" -v
+    echo -e "\nsmbclient scans for $TARGET" | tee -a "$OUTPUT_FILE"
+    smbclient //"$TARGET"/anonymous | tee -a "$OUTPUT_FILE"
+    smbclient //"$TARGET"/guest | tee -a "$OUTPUT_FILE"
+    smbclient --no-pass -L //"$TARGET" | tee -a "$OUTPUT_FILE"
 
-echo " "
-echo "Starting crackmapexec smb for " "$1"
-echo "=========================="
-/root/.local/bin/crackmapexec smb "$1"
-/root/.local/bin/crackmapexec smb "$1" -u '' -p ''
-/root/.local/bin/crackmapexec smb "$1" -u 'guest' -p ''
-/root/.local/bin/crackmapexec smb "$1" -u '' -p '' # --share 'sharename'
+    echo -e "\nsmbmap scans for $TARGET" | tee -a "$OUTPUT_FILE"
+    smbmap -H "$TARGET" | tee -a "$OUTPUT_FILE"
+    smbmap -u '' -p '' -P 445 -H "$TARGET" | tee -a "$OUTPUT_FILE"
+    smbmap -u 'guest' -p '' -P 445 -H "$TARGET" | tee -a "$OUTPUT_FILE"
+    smbmap -u '' -p '' -H "$TARGET" -R | tee -a "$OUTPUT_FILE"
+
+    echo -e "\nnbtscan for $TARGET" | tee -a "$OUTPUT_FILE"
+    nbtscan "$TARGET" -v | tee -a "$OUTPUT_FILE"
+
+    echo -e "\ncrackmapexec scans for $TARGET" | tee -a "$OUTPUT_FILE"
+    /root/.local/bin/crackmapexec smb "$TARGET" | tee -a "$OUTPUT_FILE"
+    /root/.local/bin/crackmapexec smb "$TARGET" -u '' -p '' | tee -a "$OUTPUT_FILE"
+    /root/.local/bin/crackmapexec smb "$TARGET" -u 'guest' -p '' | tee -a "$OUTPUT_FILE"
+    # Uncomment the following line if you have a specific share to scan
+    # /root/.local/bin/crackmapexec smb "$TARGET" -u '' -p '' --share 'sharename' | tee -a "$OUTPUT_FILE"
+}
+
+# Run the scan function
+perform_scan
