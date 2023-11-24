a7640814889ceaa5c45ec670cf5ed0be482294ad
sh1v4x6c <shiva108@protonmail.com>
2023-11-24 15:35:48 +0100

    Part 2 quick is done for now.

--- 2 quick/zeroscanmsf.sh
+++ 2 quick/zeroscanmsf.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
-# Check if the user is running the script with root privileges
-if [ "$EUID" -ne 0 ]
-then
-  echo "Please run as root"
-  exit
+# Function to display usage
+usage() {
+    printf "\nSyntax: zeroscanmsf.sh <NBNAME> <Server IP>\n"
+    printf "Example: ./zeroscanmsf.sh server2016 10.10.10.20\n\n"
+}
+
+# Check for root privileges
+if [ "$(id -u)" -ne 0 ]; then
+    printf "Please run as root\n"
+    exit 1
 fi
 
 # Check if the required number of arguments was provided
-if [ $# -lt 2 ]
-then
-  echo " "
-  echo "Syntax: zeroscanmsf.sh 'NBNAME' 'server ip'" 
-  echo "Example: ./zeroscanmsf.sh server2016 10.10.10.20"
-  echo " "
-    exit
+if [ $# -ne 2 ]; then
+    printf "Incorrect number of arguments supplied\n"
+    usage
+    exit 1
 fi
 
-echo "Starting MSF and running msfscripts/zeroscanmsf.rc"
+NBNAME="$1"
+SERVER_IP="$2"
+
+# Check for required tools (msfdb and msfconsole)
+if ! command -v msfdb &> /dev/null || ! command -v msfconsole &> /dev/null; then
+    printf "Error: Required Metasploit tools not found.\n"
+    exit 1
+fi
+
+# Check if MSF script file exists
+MSF_SCRIPT="./msfscripts/zerologonscan.rc"
+if [ ! -f "$MSF_SCRIPT" ]; then
+    printf "Error: Metasploit script file %s not found.\n" "$MSF_SCRIPT"
+    exit 1
+fi
+
+printf "Starting Metasploit Database...\n"
 msfdb start
-msfconsole -q -x "set NBNAME $1; setg RHOSTS $2;resource ./msfscripts/zerologonscan.rc"
+
+printf "Starting Metasploit Console and running Zerologon scan...\n"
+msfconsole -q -x "set NBNAME $NBNAME; setg RHOSTS $SERVER_IP; resource $MSF_SCRIPT"
+
+
