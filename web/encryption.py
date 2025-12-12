e713f3800302324a794031974391335f7def17fc
sh1v4x6c <shiva108@protonmail.com>
2025-12-13 00:20:55 +0100

    feat: Implement application logging and remove unused PBKDF2HMAC import from encryption.

--- web/encryption.py
+++ web/encryption.py
@@ -4,7 +4,6 @@ Encryption utilities for sensitive data protection
 
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 import os
 
