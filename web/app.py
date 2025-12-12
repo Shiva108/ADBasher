78a13ddfc178c4846662435b26e1e6352a97e94b
sh1v4x6c <shiva108@protonmail.com>
2025-12-13 00:23:18 +0100

    fix: Add `allow_unsafe_werkzeug` parameter to `socketio.run` to resolve server startup error.

--- web/app.py
+++ web/app.py
@@ -507,7 +507,7 @@ if __name__ == '__main__':
     
     # Run with SocketIO
     try:
-        socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+        socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
     except Exception as e:
         app_logger.critical(f'Fatal error starting server: {e}')
         raise
